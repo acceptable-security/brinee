7cfb89b3d276d3187487ba6c3daee5d8f2fdf29d
Brian Smith <brian@localhost.localdomain>
2015-03-11 17:44:38 -0400

    Added the appropriate port functions.

--- include/system.h
+++ include/system.h
@@ -14,7 +14,7 @@ struct regs
     unsigned int gs, fs, es, ds;
     unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
     unsigned int int_no, err_code;
-    unsigned int eip, cs, eflags, useresp, ss;    
+    unsigned int eip, cs, eflags, useresp, ss;
 };
 
 /* MAIN.C */
@@ -24,6 +24,8 @@ extern unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t 
 extern size_t strlen(const char *str);
 extern unsigned char inportb (unsigned short _port);
 extern void outportb (unsigned short _port, unsigned char _data);
+extern unsigned long inportl (unsigned short _port);
+extern void outportl (unsigned short _port, unsigned long _data);
 
 /* CONSOLE.C */
 extern void init_video(void);
--- src/std/io.c
+++ src/std/io.c
@@ -11,3 +11,13 @@ unsigned char inportb (unsigned short _port) {
 void outportb (unsigned short _port, unsigned char _data) {
     __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
 }
+
+unsigned long inportl (unsigned short _port) {
+    unsigned long rv;
+    __asm__ __volatile__ ("inl %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
+}
+
+void outportl (unsigned short _port, unsigned long _data) {
+    __asm__ __volatile__ ("outl %1, %0" : : "dN" (_port), "a" (_data));
+}
